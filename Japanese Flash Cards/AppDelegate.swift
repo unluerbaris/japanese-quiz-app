6cf5b14e995069197b1665c7d0f8e7b2374276ca
Baris Unluer <unluerbaris@gmail.com>
2021-07-10 18:07:57 +0900

    core data setup

--- Japanese Flash Cards/AppDelegate.swift
+++ Japanese Flash Cards/AppDelegate.swift
@@ -6,11 +6,10 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -30,7 +29,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+            self.saveContext()
+    }
+     
+    // MARK: - Core Data stack
+     
+    lazy var persistentContainer: NSPersistentContainer = {
+ 
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+ 
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+ 
+    // MARK: - Core Data Saving support
+ 
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+ 
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
 
