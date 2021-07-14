10c80bfe0a653677754eb788d8844168191ce975
Baris Unluer <unluerbaris@gmail.com>
2021-07-14 22:24:48 +0900

    create questions

--- Japanese Flash Cards/Data/Seeds.swift
+++ Japanese Flash Cards/Data/Seeds.swift
@@ -29,9 +29,27 @@ class Seeds {
         
         for quiz in quizArray! {
             
+            let question0 = Question(context: context)
+            question0.text = "月"
+            question0.answers = ["つき","き","にち","に"]
+            question0.correctAnswer = "つき"
+            
+            let question1 = Question(context: context)
+            question1.text = "日"
+            question1.answers = ["き","ひ","ち","は"]
+            question1.correctAnswer = "ひ"
+            
+            let question2 = Question(context: context)
+            question2.text = "人"
+            question2.answers = ["ひと","き","つき","ひげ"]
+            question2.correctAnswer = "ひと"
+            
             let quiz0 = Quiz(context: context)
             quiz0.isSuccessful = false
             quiz0.quizIndex = 0
+            quiz0.addToQuestions(question0)
+            quiz0.addToQuestions(question1)
+            quiz0.addToQuestions(question2)
             
             if quiz0.quizIndex != quiz.quizIndex {
                 saveQuiz()
